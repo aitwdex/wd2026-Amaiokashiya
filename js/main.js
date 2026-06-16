6eef1af50a7e70d13b4f71650f3f291b2618c099
yura <123056634+Amaiokashiya@users.noreply.github.com>
2026-06-16 11:38:49 +0900

    JS追加

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-// ボタンをクリックすると、カードがふわっと消えたり出たりする
-$("#toggleBtn").on("click", () => {
-  $("#card").fadeToggle();   // 表示/非表示をアニメーション付きで切り替え
+//自作モジュールのインポート
+import{getWeather,showWeather}from"./rib.js";
+
+// ボタンを押したら天気を取りに行く
+document.getElementById("getBtn").addEventListener("click",async()=>{ //awaitを利用するためasync
+  const data =await getWeather(); //APIを利用するためawait
+  showWeather(data.current_weather);
 });
